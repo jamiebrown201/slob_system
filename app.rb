164923c512debe78d6d2c4e0a71513d5c160133c
Jamie Brown <jamiebrown@notonthehighstreet.com>
2016-09-12 10:12:08 +0100

    change to modular style

--- app.rb
+++ app.rb
@@ -1,21 +1,7 @@
-require 'rubygems'
-require 'bundler'
+require "sinatra/base"
 
-Bundler.require
-
-module Slob
-  class App < Sinatra::Application
-    configure do
-      disable :method_override
-      disable :static
-
-      set :sessions,
-          :httponly     => true,
-          :secure       => production?,
-          :expire_after => 31557600, # 1 year
-          :secret       => ENV['SESSION_SECRET']
-    end
-
-    use Rack::Deflater
+class MyApp < Sinatra::Base
+  get '/' do
+    "Hello from MyApp!"
   end
 end
